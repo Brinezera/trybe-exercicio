9cd588d5085f1b104360900983efcda3feef0afb
andrebrine11@gmail.com <andrebrine11@gmail.com>
2023-05-30 14:48:38 -0300

    Exerc 2

--- 1-fundamentos/secao-06-introducao-a-javascript-es6-&-testes-unitarios/dia-01-fluxo-de-exceções/script.js
+++ 1-fundamentos/secao-06-introducao-a-javascript-es6-&-testes-unitarios/dia-01-fluxo-de-exceções/script.js
@@ -19,3 +19,27 @@ const findPersonByName = (name) => {
 };
 console.log(findPersonByName('Ana Santos'));
 console.log(findPersonByName('Ana Silva'));
+
+const arrayValidation = (array) => {
+  if (array.length === 0) {
+    throw new Error('Ops, nenhuma pessoa mora nesse estado, tente outro');
+  }
+};
+
+const findPeopleByState = (state) => {
+  try {
+    let people = [];
+    for (let index = 0; index < clients.length; index += 1) {
+      if (clients[index].address.state === state) {
+        people.push(clients[index].name);
+      }
+    }
+    arrayValidation(people);
+    return people;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+console.log(findPeopleByState('SP'));
+console.log(findPeopleByState('AC'));
